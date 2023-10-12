e931581772c308896f8914c482747427433a541f
mtriba <triba@onet.pl>
2023-10-12 15:58:24 +0200

    project initialization

---
+++ src/main/java/com/example/metodyprg1/Metodyprg1Application.java
@@ -0,0 +1,13 @@
+package com.example.metodyprg1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Metodyprg1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Metodyprg1Application.class, args);
+    }
+
+}
